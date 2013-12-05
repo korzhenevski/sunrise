89115df9b0fb7edc1925b5f72b84aaed1e954a7f
vagrant <vagrant@precise64.(none)>
2013-12-06 02:59:08 +0400

    extjs draft admin. record tapes.

--- admin/app.py
+++ admin/app.py
@@ -2,34 +2,23 @@
 # -*- coding: utf-8 -*-
 
 import pymongo
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 from datetime import datetime
 app = Flask(__name__)
 
 db = pymongo.Connection()['test']
 
-@app.template_filter()
-def format_ts(ts):
-	return datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%I:%S')
-
-@app.template_filter()
-def queue_html_class(s):
-    c = ''
-    if s.get('task_id'):
-        c = 'active'
-
-    if s.get('record') and c:
-        c = 'warning'
-
-    if s.get('retry_ivl'):
-        c = 'danger'
-
-    return c
-
 @app.route("/")
 def hello():
-    stat = db.queue.find()
-    return render_template('admin.html', stat=stat)
+    return render_template('admin.html')
+
+@app.route("/queue.json")
+def api_queue():
+    results = list(db.queue.find(fields={'runtime': 0, 'opresult': 0}))
+    return jsonify({
+        'success': True,
+        'results': results
+    })
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=13017, debug=True)
